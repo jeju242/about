532a1ca4b63de3df323c818c4f1cc583c3993927
unknown <claraqn@naver.com>
2021-01-11 20:05:03 +0900

    file

---
+++ js/map.js
@@ -0,0 +1,74 @@
+
+var mapContainer = document.getElementById('Jmap'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(33.454484, 126.562123), // 지도의 중심좌표
+        level: 4 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 마커가 표시될 위치입니다 
+var markerPosition = new kakao.maps.LatLng(33.454702, 126.565005);
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+
+var iwContent = '<div class="overlaybox">' +
+    '    <div class="boxtitle">금주 영화순위</div>' +
+    '    <div class="first">' +
+    '        <div class="triangle text">1</div>' +
+    '        <div class="movietitle text">드래곤 길들이기2</div>' +
+    '    </div>' +
+    '    <ul>' +
+    '        <li class="up">' +
+    '            <span class="number">2</span>' +
+    '            <span class="title">명량</span>' +
+    '            <span class="arrow up"></span>' +
+    '            <span class="count">2</span>' +
+    '        </li>' +
+    '        <li>' +
+    '            <span class="number">3</span>' +
+    '            <span class="title">해적(바다로 간 산적)</span>' +
+    '            <span class="arrow up"></span>' +
+    '            <span class="count">6</span>' +
+    '        </li>' +
+    '        <li>' +
+    '            <span class="number">4</span>' +
+    '            <span class="title">해무</span>' +
+    '            <span class="arrow up"></span>' +
+    '            <span class="count">3</span>' +
+    '        </li>' +
+    '        <li>' +
+    '            <span class="number">5</span>' +
+    '            <span class="title">안녕, 헤이즐</span>' +
+    '            <span class="arrow down"></span>' +
+    '            <span class="count">1</span>' +
+    '        </li>' +
+    '    </ul>' +
+    '</div>';
+iwPosition = new kakao.maps.LatLng(1, 1); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    content: iwContent,
+    borderWidth: 0
+});
+
+// 마커에 마우스오버 이벤트를 등록합니다
+kakao.maps.event.addListener(marker, 'mouseover', function () {
+    // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+    infowindow.open(map, marker);
+});
+
+// 마커에 마우스아웃 이벤트를 등록합니다
+kakao.maps.event.addListener(marker, 'mouseout', function () {
+    // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+    infowindow.close();
+});
